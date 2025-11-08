3c7ee15cfd47e60bdc309d15c20253e57b190e5b
Maxwell D. <bagels0009@gmail.com>
2025-11-07 23:19:26 -0500

    secondary commit

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const three = require('three');
+const THREE = require('three');
 const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
-
 
 app.listen(3000, function () {
     console.log("server started at 3000");
@@ -20,3 +16,31 @@ mongoose.connect('mongodb://localhost:27017/movieDB', {})
     .then(function (db) {
         console.log("db connected");
     });
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+
+//https://threejs.org/manual/#en/creating-a-scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+
+//animation loop
+function animate() {
+    renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
