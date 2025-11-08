0aa30010b90c46d314be872889a11643841cad0e
Maxwellwellium <bagels0009@gmail.com>
2025-11-08 00:11:13 -0500

    boilerplate functionality

--- server.js
+++ server.js
@@ -12,35 +12,11 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-// mongoose.connect('mongodb://localhost:27017/movieDB', {})
-//     .then(function (db) {
-//         console.log("db connected");
-//     });
+mongoose.connect('mongodb://localhost:27017/movieDB', {})
+    .then(function (db) {
+        console.log("db connected");
+    });
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
-
-//testing 10
-//https://threejs.org/manual/#en/creating-a-scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-
-
-//animation loop
-function animate() {
-    renderer.render( scene, camera );
-}
-renderer.setAnimationLoop( animate );
