c6ab225a980ad785a54ee90fbac7b8f4d28e6018
Maxwellwellium <bagels0009@gmail.com>
2025-11-09 08:23:07 -0500

    working object creation

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         proxy: {
             '/api': 'http://127.0.0.1:3000', // Proxy API requests to the Node.js server
             '/create-Container': 'http://127.0.0.1:3000', // Proxy API requests to the Node.js server
+            '/create-Rock': 'http://127.0.0.1:3000',
         },
     },
 });
